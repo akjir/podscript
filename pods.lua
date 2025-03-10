8f65ae42b7f820fad819f6401055868dcabe42d1
Stefan Stark <stefan@starknet.one>
2025-03-10 19:31:58 +0100

    change copyright year to 2025

--- pods.lua
+++ pods.lua
@@ -1,7 +1,7 @@
 --[[
 
 PodScript
-Copyright (C) 2024  Stefan Stark
+Copyright (C) 2025  Stefan Stark
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
