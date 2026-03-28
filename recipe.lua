4b7e8d4063828949a1a09e7ffcbfa2492b2716bf
Stefan Stark <stefan@starknet.one>
2026-03-28 11:15:18 +0100

    feat: add container configuration options and support for multiple container definitions in recipe.lua

--- recipe.lua
+++ recipe.lua
@@ -22,8 +22,38 @@ return {
     },
     containers = {
         { -- 1
+            -- Absolute name of the container
             name = "simple",
+            -- Detached mode.
+            detach = true,
+            -- Restart policy.
+            restart = "never",
+            -- Image to use.
             image = "simple:latest",
+        },
+        { -- 2
+            -- Missing name will use pod name
+            -- Will be pod_name-2
+            detach = true,
+            restart = "always",
+            -- Separate registry for this container.
+            registry = "another-registry.io",
+            image = "simple2:latest",
+            volumes = {
+                { "file.conf", "/path/file.conf", "ro,Z" },
+                { "folder",    "/path/folder",    "Z" },
+            },
+        },
+        {
+            -- Relative name for container
+            -- Will be pod_name-db
+            name = "*db",
+            detach = true,
+            restart = "on-failure",
+            image = "simple3:1.1",
+            options = {
+                "--env ADMIN_TOKEN='1234567890'",
+            },
         },
     },
 }
